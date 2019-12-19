7e01c9633ea6e95d48e4721793c350ddec25c0c1
jaenudin0901 <jaenudinj913198@gmail.com>
2019-12-19 23:50:35 +0700

    mengubah file script.js

--- 2.2/script.js
+++ 2.2/script.js
@@ -1,11 +1,61 @@
-function init() {
-	let nama ='Jaenudin';
-	// let umur = '28';
-	function tampilNama() {
-		console.log(nama);
-		// console.log(umur);
-	}
+// function init() {
+// 	let nama ='Jaenudin';
+// 	// let umur = '28';
+// 	function tampilNama() {
+// 		console.log(nama);
+// 		// console.log(umur);
+// 	}
 
-	console.dir(tampilNama);
-}
-init();
+// 	console.dir(tampilNama);
+// }
+// init();
+
+
+
+
+// function ucapkanSalam(waktu){
+// 	return function (nama){
+// 		console.log(`Halo ${nama}, Selamat ${waktu}, semoga hari menyenangkan!`);
+// 	}
+// }
+
+
+// let SelamatPagi = ucapkanSalam('pagi');
+// let SelamatSore = ucapkanSalam('Sore');
+// let SelamatMalam = ucapkanSalam('Malam');
+
+
+// SelamatPagi('Jaenudin');
+// SelamatMalan('safa');
+
+// console.dir(SelamatMalam('Jaenudin'));
+
+
+// contoh clasik
+
+
+
+let add = (function() {
+
+let counter = 0;
+
+	return function(){
+
+return ++counter;
+	} 
+})();
+
+
+
+counter =10;
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
