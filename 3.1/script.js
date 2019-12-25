fa4862c15a95796a4996a364864d6bca4111dbe3
jaenudin0901 <jaenudinj913198@gmail.com>
2019-12-25 14:41:46 +0700

    menambahkan folder 3.1

---
+++ 3.1/script.js
@@ -0,0 +1,13 @@
+// const tampilNama = (nama) => { return `Hallo, ${nama}`;}
+
+// console.log('Jaenudin');
+
+// const tampilNama = (nama, waktu) => {
+// 	return `selamat ${waktu}, ${nama}`;
+// }
+
+// console.log(tampilNama('jaenudin', 'siang'));
+
+const tampilNama = nama => `Hallo, ${nama}`;
+
+console.log('Jaenudin');
