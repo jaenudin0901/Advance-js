5ad80e607ef1bf19aadf77c84a0fc2b8bb3c730d
jaenudin0901 <jaenudinj913198@gmail.com>
2019-12-29 12:56:10 +0700

    mengubah beberapa paragraf

--- 3.1/script.js
+++ 3.1/script.js
@@ -8,6 +8,22 @@
 
 // console.log(tampilNama('jaenudin', 'siang'));
 
-const tampilNama = nama => `Hallo, ${nama}`;
+// const tampilNama = nama => `Hallo, ${nama}`;
 
-console.log('Jaenudin');
+// console.log('Jaenudin');
+
+
+// const tampilNama = () => `Hello Jaenudin`;
+// console.log(tampilNama());
+
+let mahasiswa =['Jaenudin','safa myesa qurratuaini', 'dwi nuryati'];
+// let jumlahHuruf = mahasiswa.map(function(nama) {
+// 	return nama.length;
+// });
+// console.log(jumlahHuruf);
+
+// let jumlahHuruf = mahasiswa.map( nama => nama.length);
+// console.log(jumlahHuruf);
+
+let jumlahHuruf = mahasiswa.map(nama =>({ nama, jmlHuruf: nama.length}));
+console.table(jumlahHuruf);
