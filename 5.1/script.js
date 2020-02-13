93f9518b83683679a65495efb03b8678abe70ea6
jaenudin0901 <jaenudinj913198@gmail.com>
2020-02-13 23:09:56 +0700

    menambahkan folder 5.1, file index.html & script.js

---
+++ 5.1/script.js
@@ -0,0 +1,13 @@
+// Template Literals /Template String
+
+const nama ='Jaenudin';
+const umur = 33;
+
+// console.log(`Hallo, nama saya ${nama}, dan saya ${umur} tahun.`);
+
+// console.log('Halo, nama saya ' + nama +' , dan saya ' + umur + ' tahun .');
+
+
+// Embedded Expressions
+console.log(`${1+1}`);
+// console.log(`${alert('halo!')}`);
